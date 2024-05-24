b8db4a9f1468fd4f407abc4ab3def19f02cdd142
Vocales22 <20233tn036@utez.edu.mx>
2024-05-24 10:47:19 -0600

    Añadir Juan.java en la rama de Juan

---
+++ Pedro.java
@@ -0,0 +1,9 @@
+package utez.edu.mx;
+
+public class Pedro {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hola mundo, pedro");
+    }
+}
